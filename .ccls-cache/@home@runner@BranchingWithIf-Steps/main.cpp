7f29476d0558c170c32ad27c4ed5e584f5e1a93e
ieee754 <94340126+brinikki@users.noreply.github.com>
2022-08-23 17:07:59 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@BranchingWithIf-Steps/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int num = 3;
+char letter = 'B';
+
+if (num > 5)
+{cout << "Number exceeds five" << endl ;}
+else
+{ cout << "Number is five or less" << endl ;}
+
+if (letter == 'A') { cout << "Letter is A" << endl;}
+
+
+
+
+
+ return 0;
+}
---
+++ .ccls-cache/@home@runner@BranchingWithIf/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int num = 8;
+char letter = 'A';
+
+if (num > 5)
+{cout << "Number exceeds five" << endl ;}
+else
+{                                     }
+
+
+
+
+
+
+
+  return 0;
+}
